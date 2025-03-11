4a3de8bd559e407cb76fa58df2768d545b3452fa
Gonzalo Gálvez Bobadilla <ggalvezb@minvu.cl>
2025-03-11 10:59:44 -0300

    intento 3

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 // Servir archivos estáticos desde "dist"
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Manejar todas las rutas y devolver "index.html"
+// Manejar todas las rutas de Vue (SPA)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
